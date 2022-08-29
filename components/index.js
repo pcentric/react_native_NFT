63589a40dfdf3c07eb79a7d14a89c046a7699031
pcentric <priyanshu.s1619@gmail.com>
2022-08-29 12:25:33 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,22 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailBids from "./DetailBids";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailDesc from './DetailDesc';
+
+export {
+  HomeHeader,
+  NFTCard,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailBids,
+  FocusedStatusBar,
+  DetailDesc,
+};
